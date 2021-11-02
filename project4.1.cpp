2e7d868a7c079372ec3335812b539579bfc5672a
Irunka1306 <92487724+Irunka1306@users.noreply.github.com>
2021-11-03 00:13:28 +0200

    Add files via upload

---
+++ project4.1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int N, k;
+	double P;
+
+	cout << "N = "; cin >> N;
+
+	P = 1;
+	k = 1;
+	while (k <= N)
+	{
+		P *= (k * k) / (N * N) * cos((N * N) / (k * k));
+		k++;
+	}
+	cout << P << endl;
+
+	P = 1;
+	k = N;
+	do
+	{
+		P *= (k * k) / (N * N) * cos((N * N) / (k * k));
+		k++;
+	} while (k <= N);
+	cout << P << endl;
+
+	P = 1;
+	for (k = 1; k <= N; k++)
+	{
+		P *= (k * k) / (N * N) * cos((N * N) / (k * k));
+	}
+	cout << P << endl;
+
+	P = 1;
+	for (k = N; k >= 1; k--)
+	{
+		P *= (k * k) / (N * N) * cos((N * N) / (k * k));
+	}
+	cout << P << endl;
+
+	return 0;
+}
+
+
